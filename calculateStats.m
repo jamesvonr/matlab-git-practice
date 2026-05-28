b4726b9c80d8a484aacc126044dcbcc47f1e6d9c
jamesvonr <jamesvonr25@gmail.com>
2026-05-28 15:16:09 -0400

    Initial matlab stats demo

---
+++ calculateStats.m
@@ -0,0 +1,5 @@
+% calculateStats.m
+function stats = calculateStats(x)
+    stats.meanValue = mean(x);
+    stats.maxValue = max(x);
+end
---
+++ runDemo.m
@@ -0,0 +1,6 @@
+% runDemo.m
+data = [4, 8, 15, 16, 23, 42];
+
+stats = calculateStats(data);
+
+disp(stats)
