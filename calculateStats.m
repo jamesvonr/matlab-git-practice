c0dd89f31e983334e347c21a40f0a659ba0fc18a
jamesvonr <jamesvonr25@gmail.com>
2026-06-04 21:59:19 -0400

    Change median calculation mode

--- calculateStats.m
+++ calculateStats.m
@@ -2,7 +2,7 @@
 function stats = calculateStats(x)
     % Compute the arithmetic mean
     stats.averageValue = mean(x);
-    stats.medianValue = median(x);
+    stats.medianValue = median(x, "all");
     stats.maxValue = max(x);
     stats.minValue = min(x);
     stats.stdValue = std(x);
