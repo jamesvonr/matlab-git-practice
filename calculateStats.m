bd63ecd4e34e429f8427f508d116d829d33fcc66
jamesvonr <jamesvonr25@gmail.com>
2026-06-04 21:51:53 -0400

    Add median value to stats

--- calculateStats.m
+++ calculateStats.m
@@ -1,7 +1,8 @@
 % calculateStats.m
 function stats = calculateStats(x)
     % Compute the arithmetic mean
-    stats.meanValue = mean(x);
+    stats.averageValue = mean(x);
+    stats.medianValue = median(x);
     stats.maxValue = max(x);
     stats.minValue = min(x);
     stats.stdValue = std(x);
